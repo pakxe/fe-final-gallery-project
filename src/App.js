800d754973adeea35b9d4b359060284b3b066c81
pakxe <pigkill40@naver.com>
2023-06-20 00:53:13 +0900

    feat: 페이지 라우팅

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import ArticlePage from './pages/ArticlePage';
+import ErrorPage from './pages/ErrorPage';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/article/:articleId' element={<ArticlePage />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
