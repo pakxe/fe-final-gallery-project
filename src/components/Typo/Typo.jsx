0d1a61ef5193a3216aefdd005c628558da5aa28a
pakxe <pigkill40@naver.com>
2023-06-20 02:06:31 +0900

    feat: 글자를 적는 Typo 컴포넌트 작성

---
+++ src/components/Typo/Typo.jsx
@@ -0,0 +1,16 @@
+import { styled } from 'styled-components';
+
+const Typo = styled.p`
+  color: ${(props) => (props.color ? props.theme.colors[props.color] : props.theme.colors.black)};
+  font-size: ${(props) => props.size || '1rem'};
+  font-weight: ${(props) => props.weight || 'normal'};
+
+  ${(props) => props.title && props.theme.font.title};
+  ${(props) => props.largeTitle && props.theme.font.largeTitle};
+  ${(props) => props.small && props.theme.font.small};
+  ${(props) => props.sub && props.theme.font.sub};
+  ${(props) => props.submit && props.theme.font.submit};
+  ${(props) => props.bold && props.theme.font.bold};
+`;
+
+export default Typo;
