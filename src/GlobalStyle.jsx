54a5b97b3ac2de885ace8cecef421c2b8c09a25b
pakxe <pigkill40@naver.com>
2023-06-20 15:15:02 +0900

    style: 전체 높이가 적용되지 않는 오류 수정

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -6,7 +6,7 @@ const GlobalStyle = createGlobalStyle`
 
   html, body, #root {
       width: 100%;
-      height: 100%;
+      min-height: 100vh;
       background-color: ${(props) => props.theme.colors.gray};
       justify-content: center;
       display: flex;
