d219686d149abcada75a8bb9858053d1d9c77cc3
pakxe <pigkill40@naver.com>
2023-06-20 17:30:59 +0900

    feat: 댓글 작성하는 컴포넌트 작성

---
+++ src/components/AddComment/AddComment.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Button from '../Button/Button';
+import Input from '../Input/Input';
+import HorizontalLine from '../HorizontalLine/HorizontalLine';
+import { styled } from 'styled-components';
+
+const AddCommentWrapper = styled.div`
+  display: flex;
+  width: 80%;
+  justify-content: space-between;
+  border-bottom: 1px solid ${(props) => props.theme.colors.lightGray};
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+// id props
+const AddComment = (props) => {
+  const [comment, setComment] = useState('');
+
+  const handleComment = ({ target }) => {
+    setComment(target.value);
+  };
+
+  // TODO: 아이디 받아서 댓글 게시하는..
+
+  return (
+    <>
+      <AddCommentWrapper>
+        <Input value={comment} onChange={handleComment} placeholder='댓글 작성...' />
+        <Button type='submit'>게시</Button>
+      </AddCommentWrapper>
+    </>
+  );
+};
+
+export default AddComment;
