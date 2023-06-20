a10dc5fc9320e38c2a166f7b16ea96152410ab95
pakxe <pigkill40@naver.com>
2023-06-20 18:22:09 +0900

    feat: ArticlePage 컴포넌트 작성

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import SelectedArticle from '../components/SelectedArticle/SelectedArticle';
+import { styled } from 'styled-components';
+import AddComment from '../components/AddComment/AddComment';
+import CommentList from '../components/CommentList/CommentList';
 
-const ArticlePage = () => {
-  return <div>articlePage</div>;
+const ArticleWrapper = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+// id를 받아서
+const ArticlePage = (props) => {
+  return (
+    <ArticleWrapper>
+      <SelectedArticle id={props.id} />
+      <AddComment />
+      <CommentList />
+    </ArticleWrapper>
+  );
 };
 
 export default ArticlePage;
