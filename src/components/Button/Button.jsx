4d82276c26bd3e84646ffca2b57b2e3da27e6f48
pakxe <pigkill40@naver.com>
2023-06-20 18:20:41 +0900

    style: 버튼에 여백 추가

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,27 +5,27 @@ import Typo from '../Typo/Typo';
 const buttonType = (type) => {
   if (type === 'submit') {
     return css`
-      padding: 10px 14px;
       color: ${(props) => props.theme.colors.blue};
       font-weight: 600;
     `;
   }
   if (type === 'sub') {
     return css`
-      padding: 8px 12px;
       color: ${(props) => props.theme.colors.gray};
     `;
   }
 };
 
 const StyledButton = styled.button`
+  padding: 10px 16px;
+  cursor: pointer;
   border: none;
   background-color: transparent;
   ${(props) => buttonType(props.type)};
 `;
 
-// submit, sub 타입을 받아서 반환하기.
 const Button = (props) => {
+  // TODO: onclick
   return <StyledButton type={props.type}>{props.children}</StyledButton>;
 };
 
